8df4a0fec240563db8edd69093fef1eac420b881
Po Chien Yang <ypochien@gmail.com>
2016-11-14 10:17:02 +0800

    重購 test case 1. rename method Add -> add

--- test_profit.py
+++ test_profit.py
@@ -6,7 +6,7 @@ class ProfitCalc(object):
     def __init__(self):
         self.content = {}
     
-    def Add(self, code, qty, price):
+    def add(self, code, qty, price):
         if code in self.content.keys():
             sum_price = self.content[code]['PRICE'] * self.content[code]['QTY'] + price
             self.content[code]['QTY'] += qty
@@ -21,11 +21,13 @@ class ProfitCalc(object):
 class ProfitTestCase(unittest.TestCase):
     def test_收到成交回報的紀錄商品_口數_價格能累計(self):
         sut = ProfitCalc()
-        sut.Add('TXO', 1, 9000)
-        sut.Add('TXO', 1, 9002)
-        expect = {"TXO": {'QTY': 2, 'PRICE': (9000 + 9002 )/ 2}}
+        sut.add('TXO', 1, 9000)
+        sut.add('TXO', 1, 9002)
+        expect = {"TXO": {'QTY': 2, 'PRICE': (9000 + 9002) / 2}}
         actual = sut.summary()
         self.assertDictEqual(expect, actual)
         
         if __name__ == '__main__':
             unittest.main()
+
+
