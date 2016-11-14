05f93e4dfbcf377cea29aba67386385039d3556e
Po Chien Yang <ypochien@gmail.com>
2016-11-14 10:14:20 +0800

    GREEN test case 1.     def test_收到成交回報的紀錄商品_口數_價格能累計(self):

--- test_profit.py
+++ test_profit.py
@@ -2,13 +2,30 @@
 import unittest
 
 
+class ProfitCalc(object):
+    def __init__(self):
+        self.content = {}
+    
+    def Add(self, code, qty, price):
+        if code in self.content.keys():
+            sum_price = self.content[code]['PRICE'] * self.content[code]['QTY'] + price
+            self.content[code]['QTY'] += qty
+            self.content[code]['PRICE'] = sum_price / self.content[code]['QTY']
+        else:
+            self.content[code] = {'QTY': qty, 'PRICE': price}
+    
+    def summary(self):
+        return self.content
+
+
 class ProfitTestCase(unittest.TestCase):
     def test_收到成交回報的紀錄商品_口數_價格能累計(self):
         sut = ProfitCalc()
-        sut.Add('TXO', '1', '9000')
-        sut.Add('TXO', '1', '9002')
-        expect = {"TXO": {'QTY': 2, 'PRICE': 9000 + 9002 / 2}}
-        self.assertDictEqual(expect, sut.info())
+        sut.Add('TXO', 1, 9000)
+        sut.Add('TXO', 1, 9002)
+        expect = {"TXO": {'QTY': 2, 'PRICE': (9000 + 9002 )/ 2}}
+        actual = sut.summary()
+        self.assertDictEqual(expect, actual)
         
         if __name__ == '__main__':
             unittest.main()
