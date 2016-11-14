bca6e0da2d1da4f598a82fa8d7723c969869e480
Po Chien Yang <ypochien@gmail.com>
2016-11-14 09:58:58 +0800

    RED test case 1.     def test_收到成交回報的紀錄商品_口數_價格能累計(self):

--- test_profit.py
+++ test_profit.py
@@ -7,10 +7,8 @@ class ProfitTestCase(unittest.TestCase):
         sut = ProfitCalc()
         sut.Add('TXO', '1', '9000')
         sut.Add('TXO', '1', '9002')
+        expect = {"TXO": {'QTY': 2, 'PRICE': 9000 + 9002 / 2}}
+        self.assertDictEqual(expect, sut.info())
         
-        
-        
-
-
-if __name__ == '__main__':
-    unittest.main()
+        if __name__ == '__main__':
+            unittest.main()
